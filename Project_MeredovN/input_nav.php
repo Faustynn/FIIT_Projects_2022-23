b91b2ba3f86aac0097c8a08158c6a8155b877c67
Nazar Faustynn <alglivemanager@gmail.com>
2024-02-13 12:01:36 +0100

    start2

--- Project_MeredovN/input_nav.php
+++
@@ -1,7 +0,0 @@
-<php? 
-$nav_input ='<li><a href="characteristic.php"><?php echo $Lang['CHARACTERISTIC']; ?> </a></li>
-          <li><a href="reviews.php"><?php echo $Lang['REVIEWS']; ?> </a></li>
-          <li><a href="comparison.php"><?php echo $Lang['COMPARISON']; ?> </a></li>
-          <li><a href="accessories.php"><?php echo $Lang['ACCESSORIES']; ?> </a></li>
-          <li><a href="faq.php"><?php echo $Lang['Others']; ?> </a></li>'
-          ?>
